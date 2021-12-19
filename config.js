db8b7e0bca419fac8a2626d911ce943c1b8f8237
Aquat1c <49980958+Aquat1c@users.noreply.github.com>
2021-12-19 23:53:18 +0200

    updated

--- config.js
+++ config.js
@@ -1,7 +1,6 @@
 module.exports = {
     app: {
         px: '.r ',
-        token: 'ODU2MjU2OTMxNTU5NjM2OTkz.YM-ZaA.YVEDpcyjI1u50QzVHn3TXnR4Uco',
         playing: 'Fairy Fencer F Advent Dark Force'
     },
 

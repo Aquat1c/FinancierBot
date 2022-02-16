f4bf1e880ecd59d7bcfe68997e3f9585a6f1dde6
a b <49980958+Aquat1c@users.noreply.github.com>
2022-02-16 21:00:00 +0000

    Added "Choose" command

--- commands/Etc/ratewaifu.js
+++ commands/Etc/ratewaifu.js
@@ -1,3 +1,5 @@
+       const { MessageEmbed } = require('discord.js');
+
           module.exports = {
     name: 'ratewaifu',
     aliases: ['ratewaifu'],
@@ -8,24 +10,30 @@ execute(client, message, args) {
    min = 0;
           max = 100;
           const a = Math.random() * (max - min) + min;
-          const waifu = args[0];
+          const waifu = args.join(' ');
           if (waifu === ('Noire'))
             {
-            return message.channel.send("I'll rate " + "Noire" + ' as ' + '100/100' )
+            var answer = ("I'll rate " + "Noire" + ' as ' + '100/100' )
 
             }
           if (typeof(waifu) === ('undefined'))
             {
-            return message.channel.send("Specify your waifu, retard" )
+            var answer = ("Specify your waifu, retard" )
 
             }
          else   if (waifu === ('nyamota') ||waifu === ('нямота') ||waifu === ('Nyamota') || waifu === ('Нямота')   )
             {
-            return message.channel.send("I'll rate " + "Nyamota" + ' as ' + '-1/100' )
+            var answer = ("I'll rate " + "Nyamota" + ' as ' + '-1/100' )
 
             }
           else {
-          return message.channel.send("I'll rate " + waifu + ' as ' + Math.round(a) + '/100' )
+          var answer = ("I'll rate " + waifu + ' as ' + Math.round(a) + '/100' )
           }
+          const Embed = new MessageEmbed()
+  .setTitle(' '+answer)
+ .setDescription('Waifu rater')
+ .setColor('RED');
+
+message.channel.send({ embeds: [Embed] });
 },
     };
